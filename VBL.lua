3b2c889ae3b80e163ae191debf758e615270144d
uuisudo <uusyimpri@gmail.com>
2026-07-06 09:59:11 +0800

    Update VBL.lua

--- VBL.lua
+++ VBL.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://api.luacrack.site/files/v4/loaders/9d660cffe3cbda9b288174846d8bacbc.lua"))()
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/884e97cf42c9881e24be9504015407330ca31efdf2e02ef728c32d314608446a/download"))()
